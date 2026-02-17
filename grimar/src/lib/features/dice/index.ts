9f0e0072535490642ad86a88d71e211ce06888b6
Mystery2099 <xxmysteryxxftb@gmail.com>
2026-02-17 11:12:07 -0500

    refactor: remove dice feature to simplify dependencies

--- grimar/src/lib/features/dice/index.ts
+++
@@ -1,4 +0,0 @@
-// Dice feature exports
-export { diceState, QUICK_ROLLS } from './stores/dice-store.svelte';
-export { roll, addDice, clear, isValidNotation, isReady, initDiceBox } from './services/dice-service';
-export type { RollResult, DiceNotation, DieType } from './types';
